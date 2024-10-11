f9c55048693c393283d75bb9eadd14d8b0df8417
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-10-10 22:00:38 -0300

    feat: create 'docentes' page

---
+++ frontend/app/docentes/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="h-full w-full text-center content-center">
+      <h1>Docentes</h1>
+    </div>
+  );
+}
