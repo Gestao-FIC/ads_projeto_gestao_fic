d17bdc02231c5205de493daf28bacbaec549b862
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-10-10 22:35:28 -0300

    fix: auth file name in include url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sgset.urls'))
+    path('login/', include('sgset.urls.AuthUrls'))
 ]
