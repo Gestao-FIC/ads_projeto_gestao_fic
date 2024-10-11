5c0b9183b2b86ff7efba6f06048b22b0a3bfb358
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-10-10 21:38:35 -0300

    feat: create auth serializer class

---
+++ backend/sgset/serializers/AuthSerializer.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class AuthSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
