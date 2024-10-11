e87ed0751229f6772804dad236f3f00d0236f658
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-10-10 22:01:08 -0300

    feat: add nav component to layout file

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Nav from "@/components/nav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row min-h-screen w-screen`}
       >
-        {children}
+        <Nav />
+        <div className="w-full min-h-screen">{children}</div>
       </body>
     </html>
   );
