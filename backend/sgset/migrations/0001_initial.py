2ebbf137fff59cae507e3185d677c23ed6088dac
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-10-16 21:03:54 -0300

    docs: add docstring for create superuser function

--- backend/sgset/migrations/0001_initial.py
+++ backend/sgset/migrations/0001_initial.py
@@ -1,7 +1,26 @@
-from django.db import migrations
+import uuid
+import django
 from decouple import config
+from django.db import migrations
+from django.db import models
+from django.db.migrations.state import StateApps
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
-def create_initial_superuser(apps, schema_editor):
+def create_initial_superuser(apps: StateApps, schema_editor: BaseDatabaseSchemaEditor) -> None:
+    """Creates an initial superuser using environment variables.
+
+    Args:
+        apps (Apps): Django apps registry to get models dynamically.
+        schema_editor (BaseDatabaseSchemaEditor): Allows schema alterations within migrations.
+    
+    Environment Variables:
+        - DJANGO_SUPERUSER_USERNAME: Username of the superuser (default: 'admin').
+        - DJANGO_SUPERUSER_EMAIL: Email of the superuser (default: 'admin@example.com').
+        - DJANGO_SUPERUSER_PASSWORD: Password of the superuser (default: 'adminpassword').
+
+    Returns:
+        None
+    """
     User = apps.get_model('auth', 'User')
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
@@ -19,5 +38,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_initial_superuser),
+        migrations.RunPython(create_initial_superuser)
     ]
