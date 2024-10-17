bae32aa45de47dc7c64d026b2b0fdb07448df003
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-10-17 18:32:37 -0300

    Data ETL and pipeline configuration
    Created pipeline script to download and tranformer xml data in json file

--- backend/sgset/services/DataDownloader.py
+++ backend/sgset/services/DataDownloader.py
@@ -12,126 +12,177 @@ from selenium.webdriver.support import expected_conditions as EC
 
 logging.basicConfig(level=logging.INFO)
 
-def load_configurations(config_file):
-    """Load configurations from a YAML file.
+class ConfigLoader:
+    """Class to load configurations from a YAML file."""
+    
+    @staticmethod
+    def load_configurations(config_file):
+        """Loads configurations from a specified YAML file.
 
-    Args:
-        config_file (str): Path to the YAML configuration file.
+        Args:
+            config_file (str): Path to the YAML configuration file.
 
-    Returns:
-        dict: The configurations loaded from the file.
-    """
-    with open(config_file, 'r') as f:
-        return yaml.safe_load(f)
+        Returns:
+            dict: Parsed configuration data from the YAML file.
+        """
+        with open(config_file, 'r') as f:
+            return yaml.safe_load(f)
 
-def configure_driver(edge_driver_path):
-    """Configure and create a Selenium WebDriver for Microsoft Edge.
+class EdgeDriver:
+    """Class to configure and create a Selenium WebDriver for Microsoft Edge."""
+    
+    def __init__(self, edge_driver_path):
+        """Initializes the EdgeDriver with the specified driver path.
 
-    Args:
-        edge_driver_path (str): Path to the Edge WebDriver executable.
+        Args:
+            edge_driver_path (str): Path to the Microsoft Edge WebDriver executable.
+        """
+        self.edge_driver_path = edge_driver_path
+        self.driver = self._configure_driver()
 
-    Returns:
-        webdriver.Edge: Configured Edge WebDriver instance.
-    """
-    service = Service(edge_driver_path)
-    options = Options()
-    options.add_argument('--start-maximized')
-    prefs = {
-        "download.default_directory": os.path.join(os.path.expanduser("~"), "Downloads"),
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    }
-    options.add_experimental_option("prefs", prefs)
-    return webdriver.Edge(service=service, options=options)
+    def _configure_driver(self):
+        """Configures and initializes the Edge WebDriver.
 
-def perform_scraping(driver, url):
-    """Navigate to a URL and initiate a file download.
+        Returns:
+            webdriver.Edge: Configured Edge WebDriver instance.
+        """
+        service = Service(self.edge_driver_path)
+        options = Options()
+        options.add_argument('--start-maximized')
+        prefs = {
+            "download.default_directory": os.path.join(os.path.expanduser("~"), "Downloads"),
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        }
+        options.add_experimental_option("prefs", prefs)
+        return webdriver.Edge(service=service, options=options)
 
-    Args:
-        driver (webdriver.Edge): The Selenium WebDriver instance.
-        url (str): The URL to navigate to for scraping.
+    def get_driver(self):
+        """Retrieves the configured Edge WebDriver instance.
 
-    Raises:
-        Exception: If an error occurs while trying to download the file.
-    """
-    try:
-        driver.get(url)
-        wait = WebDriverWait(driver, 20)
-        download_button = wait.until(EC.element_to_be_clickable((By.ID, 'imgExportarXLS')))
-        download_button.click()
-        logging.info("Download initiated successfully.")
-    except Exception as e:
-        logging.error(f"Error trying to download the file: {e}")
-        raise
+        Returns:
+            webdriver.Edge: The Edge WebDriver instance.
+        """
+        return self.driver
 
-def find_recent_file(download_folder):
-    """Find the most recently downloaded file in a specified folder.
+    def quit(self):
+        """Quits the Edge WebDriver, closing all associated windows."""
+        self.driver.quit()
 
-    Args:
-        download_folder (str): The folder to search for downloaded files.
+class Scraper:
+    """Class to perform web scraping and file downloads."""
+    
+    def __init__(self, driver):
+        """Initializes the Scraper with the specified WebDriver.
 
-    Returns:
-        str: The path to the most recent file, or None if no files found.
-    """
-    files = [f for f in os.listdir(download_folder) if not f.endswith('.crdownload')]
-    file_paths = [os.path.join(download_folder, file) for file in files]
-    return max(file_paths, key=os.path.getctime, default=None)
+        Args:
+            driver (webdriver.Edge): The Edge WebDriver instance to use for scraping.
+        """
+        self.driver = driver
 
-def move_file(source_file, destination):
-    """Move a file from the source location to the destination.
+    def perform_scraping(self, url):
+        """Performs the scraping operation by navigating to the given URL and initiating a download.
 
-    Args:
-        source_file (str): Path to the source file to move.
-        destination (str): Path to the destination folder.
+        Args:
+            url (str): The URL to navigate to for scraping.
 
-    Raises:
-        Exception: If moving the file fails.
-    """
-    shutil.move(source_file, destination)
-    logging.info(f"File {os.path.basename(source_file)} moved to {destination} successfully.")
+        Raises:
+            Exception: If there is an error during the scraping or download process.
+        """
+        try:
+            self.driver.get(url)
+            wait = WebDriverWait(self.driver, 20)
+            download_button = wait.until(EC.element_to_be_clickable((By.ID, 'imgExportarXLS')))
+            download_button.click()
+            logging.info("Download initiated successfully.")
+        except Exception as e:
+            logging.error(f"Error trying to download the file: {e}")
+            raise
 
-def wait_for_download_completion(download_folder, timeout):
-    """Wait until all downloads in the specified folder are complete.
+class FileHandler:
+    """Class to manage file operations such as moving and finding files."""
+    
+    @staticmethod
+    def find_recent_file(download_folder):
+        """Finds the most recently downloaded file in the specified download folder.
 
-    Args:
-        download_folder (str): The folder to monitor for downloads.
-        timeout (int): Maximum time to wait for downloads to complete.
+        Args:
+            download_folder (str): Path to the folder where downloads are stored.
 
-    Raises:
-        Exception: If the timeout is exceeded before downloads complete.
-    """
-    while True:
-        initial_time = time.time()
-        time.sleep(5)
-        if not any(f.endswith('.crdownload') for f in os.listdir(download_folder)):
-            break
-        if time.time() - initial_time > timeout:
-            logging.error("Timeout for completing the download exceeded.")
-            raise Exception("Timeout for completing the download exceeded.")
-        time.sleep(1)
+        Returns:
+            str: Path to the most recently downloaded file, or None if no file is found.
+        """
+        files = [f for f in os.listdir(download_folder) if not f.endswith('.crdownload')]
+        file_paths = [os.path.join(download_folder, file) for file in files]
+        return max(file_paths, key=os.path.getctime, default=None)
 
-def pipeline_step(url, edge_driver_path, destination_folder, download_folder, timeout):
-    """Perform the complete pipeline for scraping and file handling.
+    @staticmethod
+    def move_file(source_file, destination):
+        """Moves a file from the source path to the specified destination.
 
-    Args:
-        url (str): The URL to scrape.
-        edge_driver_path (str): Path to the Edge WebDriver executable.
-        destination_folder (str): Folder to move the downloaded file to.
-        download_folder (str): Folder where files are downloaded.
-        timeout (int): Maximum time to wait for the download to complete.
+        Args:
+            source_file (str): Path to the source file to be moved.
+            destination (str): Path to the destination folder.
 
-    Raises:
-        Exception: If an error occurs during the scraping or file handling process.
-    """
-    driver = configure_driver(edge_driver_path)
-    try:
-        perform_scraping(driver, url)
-        wait_for_download_completion(download_folder, timeout)
-        recent_file = find_recent_file(download_folder)
-        if recent_file:
-            move_file(recent_file, destination_folder)
-        else:
-            logging.warning("No file found to move.")
-    finally:
-        driver.quit()
+        Raises:
+            Exception: If the move operation fails.
+        """
+        shutil.move(source_file, destination)
+        logging.info(f"File {os.path.basename(source_file)} moved to {destination} successfully.")
+
+    @staticmethod
+    def wait_for_download_completion(download_folder, timeout):
+        """Waits for the download to complete, checking for incomplete download files.
+
+        Args:
+            download_folder (str): Path to the folder where downloads are stored.
+            timeout (int): Maximum time to wait for the download to complete in seconds.
+
+        Raises:
+            Exception: If the timeout for completing the download is exceeded.
+        """
+        while True:
+            initial_time = time.time()
+            time.sleep(5)
+            if not any(f.endswith('.crdownload') for f in os.listdir(download_folder)):
+                break
+            if time.time() - initial_time > timeout:
+                logging.error("Timeout for completing the download exceeded.")
+                raise Exception("Timeout for completing the download exceeded.")
+            time.sleep(1)
+
+class Pipeline:
+    """Class to execute the complete scraping and file handling pipeline."""
+    
+    def __init__(self, url, edge_driver_path, destination_folder, download_folder, timeout):
+        """Initializes the Pipeline with the necessary parameters.
+
+        Args:
+            url (str): The URL to scrape.
+            edge_driver_path (str): Path to the Microsoft Edge WebDriver executable.
+            destination_folder (str): Path to the folder where downloaded files will be moved.
+            download_folder (str): Path to the folder where downloads are stored.
+            timeout (int): Maximum time to wait for downloads to complete in seconds.
+        """
+        self.url = url
+        self.edge_driver_path = edge_driver_path
+        self.destination_folder = destination_folder
+        self.download_folder = download_folder
+        self.timeout = timeout
+
+    def run(self):
+        """Executes the scraping and file handling process."""
+        driver_manager = EdgeDriver(self.edge_driver_path)
+        driver = driver_manager.get_driver()
+        try:
+            scraper = Scraper(driver)
+            scraper.perform_scraping(self.url)
+            FileHandler.wait_for_download_completion(self.download_folder, self.timeout)
+            recent_file = FileHandler.find_recent_file(self.download_folder)
+            if recent_file:
+                FileHandler.move_file(recent_file, self.destination_folder)
+            else:
+                logging.warning("No file found to move.")
+        finally:
+            driver_manager.quit()
--- backend/sgset/services/Trigger.py
+++ backend/sgset/services/Trigger.py
@@ -1,20 +1,57 @@
 import os
-from DataDownloader import load_configurations, pipeline_step
+import logging
+from DataDownloader import ConfigLoader, Pipeline
+from Etl import ETLProcessor
+
+logging.basicConfig(level=logging.INFO)
+
+class ETLPipelineOrchestrator:
+    """Class to orchestrate the ETL and download processes."""
+    
+    def __init__(self, config_path):
+        self.config = ConfigLoader.load_configurations(config_path)
+        self.config['download_folder'] = os.path.join(os.path.expanduser("~"), "Downloads")
+
+    def check_for_xls_files(self):
+        """Check if there are any XLS/XLSX files in the destination folder."""
+        xls_files = [f for f in os.listdir(self.config['destination_folder']) if f.endswith('.xls') or f.endswith('.xlsx')]
+        return xls_files
+
+    def run_etl(self):
+        """Run the ETL process."""
+        logging.info("Arquivo XLS encontrado. Iniciando o processo de ETL.")
+        etl_processor = ETLProcessor(self.config['destination_folder'])
+        etl_processor.perform_etl()
+
+    def run_download(self):
+        """Run the file download process."""
+        logging.info("Nenhum arquivo XLS encontrado. Iniciando o download.")
+        pipeline = Pipeline(
+            url=self.config['url'],
+            edge_driver_path=self.config['edge_driver_path'],
+            destination_folder=self.config['destination_folder'],
+            download_folder=self.config['download_folder'],
+            timeout=self.config['timeout']
+        )
+        pipeline.run()
+
+    def orchestrate(self):
+        """Orchestrate the ETL and download processes."""
+        if self.check_for_xls_files():
+            self.run_etl()
+        else:
+            self.run_download()
+            
+            logging.info("Verificando arquivos XLS após o download.")
+            if self.check_for_xls_files():
+                self.run_etl()
+            else:
+                logging.error("Nenhum arquivo XLS encontrado após o download.")
 
 def main():
-    """Main function to load configurations and execute the scraping pipeline.
-    """
-    config = load_configurations('../../config.yaml')
-
-    config['download_folder'] = os.path.join(os.path.expanduser("~"), "Downloads")
-
-    pipeline_step(
-        config['url'],
-        config['edge_driver_path'],
-        config['destination_folder'],
-        config['download_folder'],
-        config['timeout']
-    )
+    """Main function to start the orchestration."""
+    orchestrator = ETLPipelineOrchestrator('../../config.yaml')
+    orchestrator.orchestrate()
 
 if __name__ == "__main__":
     main()
