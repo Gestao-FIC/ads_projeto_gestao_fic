95e109803a004b0e2adeeb38a7074b22bbafefb6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-10-17 23:08:10 -0300

    feat: update models import

--- backend/sgset/models/__init__.py
+++ backend/sgset/models/__init__.py
@@ -1,5 +1,5 @@
-from .group import Group
-from .class_day_schedule import ClassDaySchedule
+from .course_class import Group
+from .class_schedule import ClassDaySchedule
 from .course import Course
 from .instructor import Teacher
 from .day_of_week import DayOfWeek
