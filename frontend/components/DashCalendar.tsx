4f67541565431240840ac39e9b3247ef0b582061
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-11-18 19:36:28 -0300

    feat: create calendar component

--- frontend/components/DashCalendar.tsx
+++ frontend/components/DashCalendar.tsx
@@ -7,12 +7,13 @@ export default function DashCalendar() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
-    <div className="bg-primary p-4 rounded-l-2xl w-1/2">
+    <div className="bg-primary p-4 rounded-l-2xl">
       <Calendar
         mode="single"
         selected={date}
         onSelect={setDate}
-        className="rounded-md border h-full w-3/4"
+        className="rounded-md border flex justify-center items-center"
+        fixedWeeks
       />
     </div>
   );
