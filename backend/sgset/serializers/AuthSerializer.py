50ecc3f0355d95f0e444215ee4e9fcb22b4d4c82
Carlos Henrique <carlosgfkp@outook.com>
2024-11-21 22:59:36 -0300

    refactor: remove AuthSerializer file

--- backend/sgset/serializers/AuthSerializer.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-class AuthSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
