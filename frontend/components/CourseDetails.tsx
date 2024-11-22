7a410081f83d11c06b53b410e14c66f6bac87fce
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-11-21 22:28:34 -0300

    feat: linked courses table with courses detail

--- frontend/components/CourseDetails.tsx
+++ frontend/components/CourseDetails.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { courseDetails } from "@/mocks/mock";
+import { CoursesType } from "@/mocks/mock";
 import { diasSemana, iniciaisDias } from "@/mocks/mock";
 
-export function CourseDetails() {
+export function CourseDetails({ course }: { course: CoursesType | null }) {
   // Função para estilizar os dias da semana
   const renderDiasSemana = () => {
     return diasSemana.map((dia, index) => {
-      const isCursoDay = courseDetails.diaSemana.includes(dia);
+      const isCursoDay = course?.diaSemana.includes(dia);
       return (
         <span
           key={index}
@@ -20,6 +20,18 @@ export function CourseDetails() {
     });
   };
 
+  if (!course) {
+    return (
+      <div className="bg-primary h-screen p-4 text-secondary shadow-md w-80 rounded-l-2xl">
+        <h2 className="font-bold mb-4 text-xl text-white">Detalhes do Curso</h2>
+        <hr className="mb-4 border-secondary" />
+        <p className="text-center text-white">
+          Selecione um curso para ver os detalhes.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-primary h-screen p-4 text-secondary shadow-md w-80 rounded-l-2xl">
       <h2 className="font-bold mb-4 text-xl text-white">Detalhes do Curso</h2>
@@ -27,35 +39,35 @@ export function CourseDetails() {
       <div className="space-y-2">
         <p className="flex justify-between">
           <span className="font-semibold text-white">Turno:</span>{" "}
-          {courseDetails.turno === "M"
+          {course.turno === "M"
             ? "Manhã"
-            : courseDetails.turno === "T"
+            : course.turno === "T"
             ? "Tarde"
             : "Noite"}
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Carga Horária:</span>{" "}
-          <p>{courseDetails.cargaHoraria}</p>
+          <span className="font-semibold text-white">Carga Horária:</span>
+          <p>{course.cargaHoraria}</p>
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Dias da Semana:</span>{" "}
+          <span className="font-semibold text-white">Dias da Semana:</span>
           <p>{renderDiasSemana()}</p>
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Data Início:</span>{" "}
-          <p>{courseDetails.dataInicio}</p>
+          <span className="font-semibold text-white">Data Início:</span>
+          <p>{course.dataInicio}</p>
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Data Fim:</span>{" "}
-          <p>{courseDetails.dataFim}</p>
+          <span className="font-semibold text-white">Data Fim:</span>
+          <p>{course.dataFim}</p>
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Docente:</span>{" "}
-          <p>{courseDetails.docente}</p>
+          <span className="font-semibold text-white">Docente:</span>
+          <p>{course.docente}</p>
         </p>
         <p className="flex justify-between">
-          <span className="font-semibold text-white">Quórum:</span>{" "}
-          <p>{courseDetails.quorum}%</p>
+          <span className="font-semibold text-white">Quórum:</span>
+          <p>{course.quorum}%</p>
         </p>
       </div>
     </div>
