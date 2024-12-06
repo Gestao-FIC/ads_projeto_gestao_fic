ce91ccd37f3fda8eef213aceec44b4131fae2f3e
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-05 23:04:23 -0300

    feat(service): call normalizer methds in view process

--- backend/sgset/views/SGSETView.py
+++ backend/sgset/views/SGSETView.py
@@ -18,17 +18,29 @@ class SGSETView(APIView):
             df_data = SGSETNormalizer.rename_columns(df_data=df_data)
             df_data = SGSETNormalizer.remove_duplicates(df_data=df_data)
 
+            # Persist sgset rows in database
+            self._save_sgset(df_data=df_data)
+
+            # Extract course name and persist in database
+            SGSETNormalizer.persist_course_info(df_data=df_data)
+
+            # Extract class infos and persist in database
+            df_classes = SGSETNormalizer.persist_class_info(df_data=df_data)
+
             # Transform dataframe in python dict
             data = df_data.to_dict(orient='records')
-            serializer = SGSETSerializer(data=data, many=True)
 
-            # Save rows in database
-            SGSETModel.objects.all().delete()
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
+            # Get class id and relate with dayofweek
+            df_data['class_id'] = [
+                str(obj.code).replace('-', '')
+                for obj in df_classes
+            ]
 
-            # Replace day of week string for foreing key
-            # SGSETNormalizer.find_dayofweek_fk(df_data=df_data)
+            # Replace day of week string for foreing key and persist in table "class_schedule"
+            df_class_schedule = SGSETNormalizer.find_dayofweek_fk(
+                df_data=df_data
+            )
+            # SGSETNormalizer.persist_class_schedule(df_data=df_class_schedule)
 
             return Response(data)
         except ValidationError as e:
@@ -39,3 +51,20 @@ class SGSETView(APIView):
         except Exception as e:
             print(e)
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def _save_sgset(self, df_data):
+        """_summary_
+
+        Args:
+            df_data (_type_): _description_
+        """
+
+        # Transform dataframe in python dict
+        data = df_data.to_dict(orient='records')
+
+        # Save rows in database
+        serializer = SGSETSerializer(data=data, many=True)
+        SGSETModel.objects.all().delete()
+        serializer.is_valid(raise_exception=True)
+
+        return serializer.save()
