3be4d1ffd1ea4442ef1985f541b81e9ebf7c46df
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-05 21:42:06 -0300

    feat(normalizer): add method to extract and persist course in couse table

--- backend/sgset/services/SGSETNormalizer.py
+++ backend/sgset/services/SGSETNormalizer.py
@@ -1,12 +1,18 @@
 from unidecode import unidecode
-from sgset.models.day_of_week import DayOfWeek
 import pandas as pd
+from decimal import Decimal
+
+from sgset.models.course import CourseModel
+from sgset.models.day_of_week import DayOfWeekModel
+from sgset.serializers.course_serializer import CourseSerializer
+
 
 class SGSETNormalizer:
 
     @staticmethod
     def rename_columns(df_data):
-        df_data.columns = df_data.columns.str.replace(" ", "_").str.lower().map(unidecode)
+        df_data.columns = df_data.columns.str.replace(
+            " ", "_").str.lower().map(unidecode)
         return df_data
 
     @staticmethod
@@ -24,21 +30,20 @@ class SGSETNormalizer:
 
     @staticmethod
     def find_dayofweek_fk(df_data):
-        
+
         # Transform day of week table in dataframe
-        weekdays = DayOfWeek.objects.all()
+        weekdays = DayOfWeekModel.objects.all()
         days_weekdays = list(weekdays.values('id', 'name'))
         df_weekdays = pd.DataFrame(days_weekdays)
 
         # Regex to identify weekday
         weekdays_regex = {
-            "segunda-feira": r"2ª|segunda",
-            "terça-feira": r"3ª|terça",
-            "quarta-feira": r"4ª|quarta",
-            "quinta-feira": r"5ª|quinta",
-            "sexta-feira": r"6ª|sexta",
-            "sábado": r"sabado|sábado",
-            "domingo": r"domingo"
+            "Segunda-feira": r"2ª|segunda",
+            "Terça-feira": r"3ª|terça",
+            "Quarta-feira": r"4ª|quarta",
+            "Quinta-feira": r"5ª|quinta",
+            "Sexta-feira": r"6ª|sexta",
+            "Sabado": r"sabado|sábado"
         }
 
         # Relationship list
@@ -47,20 +52,51 @@ class SGSETNormalizer:
         # Identify ids
         for weekday, regex in weekdays_regex.items():
             # Filter row that correspond to regex value
-            matches_row = df_data['dia_da_semana'].str.lower().str.contains(regex, na=False)
-            print(matches_row)
+            matches_row = df_data['dia_da_semana'].str.lower(
+            ).str.contains(regex, na=False)
 
             # Get weekday ids
-            weekday_id = df_weekdays.loc[df_weekdays['name'] == weekday, 'id'].values[0]
-            print(weekday_id)
+            weekday_id = df_weekdays.loc[df_weekdays['name']
+                                         == weekday, 'id'].values[0]
 
             # Connect class id and weekday id
-            class_schedule = df_data.loc[matches_row, ['id']].copy()
-            class_schedule['day_of_week'] = weekday_id
+            class_schedule = df_data.loc[matches_row, ['class_id']].copy()
+            class_schedule['day_of_week_id'] = weekday_id
 
             class_weekdays_relationship.append(class_schedule)
 
-        turma_dias_df = pd.concat(class_weekdays_relationship, ignore_index=True)
-        turma_dias_df.head()
-         
+        return pd.concat(
+            class_weekdays_relationship, ignore_index=True)
 
+    @staticmethod
+    def persist_course_info(df_data):
+
+        # Select Curso and preco_por_participante column
+        df_data = df_data[['curso', 'preco_por_participante']]
+
+        # Remove nullable fields and duplicates
+        df_data = df_data.dropna()
+        df_data = df_data.drop_duplicates()
+
+        # Rename df columns according to model
+        column_rename = {
+            'curso': 'name',
+            'preco_por_participante': 'price_per_student'
+        }
+        df_data = df_data.rename(columns=column_rename)
+
+        # Apply lambda function to convert 'price_per_student' column to valid decimal
+        df_data['price_per_student'] = df_data['price_per_student'].apply(
+            lambda x: Decimal(x.replace(".", "").replace(",", "."))
+            if isinstance(x, str) else Decimal(x)
+        )
+
+        # Transform dataframe in python dict
+        data = df_data.to_dict(orient='records')
+
+        # Persist data
+        serializer = CourseSerializer(data=data, many=True)
+        CourseModel.objects.all().delete()
+        serializer.is_valid(raise_exception=True)
+
+        return serializer.save()
