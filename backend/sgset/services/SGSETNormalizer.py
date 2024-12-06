4a3d3d335c5098364f81cc3fef5ecb9d66188987
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-05 23:04:04 -0300

    feat(normalizer): add course class database persist

--- backend/sgset/services/SGSETNormalizer.py
+++ backend/sgset/services/SGSETNormalizer.py
@@ -1,10 +1,16 @@
+from decimal import Decimal
+from datetime import datetime
 from unidecode import unidecode
 import pandas as pd
-from decimal import Decimal
+
 
 from sgset.models.course import CourseModel
 from sgset.models.day_of_week import DayOfWeekModel
 from sgset.serializers.course_serializer import CourseSerializer
+from sgset.serializers.course_class_serializer import CourseClassModelSerializer
+from sgset.models.course_class import CourseClassModel
+from sgset.serializers.class_schedule import ClassScheduleSerializer
+from sgset.models.class_schedule import ClassScheduleModel
 
 
 class SGSETNormalizer:
@@ -69,8 +75,26 @@ class SGSETNormalizer:
             class_weekdays_relationship, ignore_index=True)
 
     @staticmethod
-    def persist_course_info(df_data):
+    def persist_class_schedule(df_data):
+        # Rename df columns according to model
+        column_rename = {
+            'class_id': 'class_instance',
+            'day_of_week_id': 'day_of_week'
+        }
+        df_data = df_data.rename(columns=column_rename)
 
+        # Transform dataframe in python dict
+        data = df_data.to_dict(orient='records')
+
+        # Save rows in databasel
+        serializer = ClassScheduleSerializer(data=data, many=True)
+        ClassScheduleModel.objects.all().delete()
+        serializer.is_valid(raise_exception=True)
+
+        return serializer.save()
+
+    @staticmethod
+    def persist_course_info(df_data):
         # Select Curso and preco_por_participante column
         df_data = df_data[['curso', 'preco_por_participante']]
 
@@ -97,6 +121,62 @@ class SGSETNormalizer:
         # Persist data
         serializer = CourseSerializer(data=data, many=True)
         CourseModel.objects.all().delete()
+        serializer.is_valid(raise_exception=True)
+
+        return serializer.save()
+
+    @staticmethod
+    def persist_class_info(df_data):
+
+        # Get unique course names from the 'curso' column of the DataFrame
+        courses_names = df_data['curso'].unique()
+
+        # Query the database to get all courses that match the names in 'courses_names'
+        courses = CourseModel.objects.filter(name__in=courses_names)
+        course_ids_dict = {course.name: course.id for course in courses}
+
+        # Map the 'curso' column to the new 'course' column with course IDs
+        df_data['course'] = df_data['curso'].map(
+            lambda x: str(course_ids_dict.get(x, "")).replace("-", "")
+            if x in course_ids_dict else ""
+        )
+
+        # Rename df columns according to model
+        column_rename = {
+            'turma': 'code',
+            'situacao': 'day_of_week',
+            'situacao': 'day_of_week',
+            'turno': "shift",
+            'carga_horaria': 'duration',
+            'no_de_matriculas_realizado': 'actual_enrollments',
+            'no_de_matriculas_estimado': 'estimated_enrollments',
+            'periodo_de': 'period_from',
+            'periodo_ate': "period_to",
+            'horario_de': 'start_time',
+            'horario_ate': 'end_time',
+            'dia_da_semana': 'day_of_week',
+            'modalidade': 'modality',
+            'atendimento': 'attendance',
+        }
+        df_data = df_data.rename(columns=column_rename)
+
+        df_data['period_from'] = df_data['period_from'].dt.date
+        df_data['period_to'] = df_data['period_to'].dt.date
+
+        # Add the 'status' column based on the conditions
+        df_data['status'] = df_data.apply(
+            lambda row:
+                'concluido' if row['period_to'] < datetime.now().date()
+                else ('programado' if row['period_to'] > datetime.now().date() else 'em_andamento'),
+            axis=1
+        )
+
+        # Transform dataframe in python dict
+        data = df_data.to_dict(orient='records')
+
+        # Persist data
+        serializer = CourseClassModelSerializer(data=data, many=True)
+        CourseClassModel.objects.all().delete()
         serializer.is_valid(raise_exception=True)
 
         return serializer.save()
