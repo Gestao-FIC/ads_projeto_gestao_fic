8f1c9e2f75cfc219244701b0b07633cd226b8be1
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-13 22:27:14 -0300

    chore(database): connect api with docker postgres instance

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,10 +92,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Password validation
